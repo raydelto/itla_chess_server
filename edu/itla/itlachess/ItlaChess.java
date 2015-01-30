2372839aed05c34eacda1db812cd38b5d3a4aaca
raydelto <raydelto@gmail.com>
2015-01-29 20:43:05 -0400

    Making server capable of receiving connections

---
+++ edu/itla/itlachess/ItlaChess.java
@@ -0,0 +1,10 @@
+package edu.itla.itlachess;
+
+import edu.itla.itlachess.network.Server;
+
+public class ItlaChess {
+	public static void main(String[] args) {
+		new Server(2222).listen();
+	}
+
+}
--- edu/itla/itlachess/network/Server.java
+++ edu/itla/itlachess/network/Server.java
@@ -1,10 +1,27 @@
 package edu.itla.itlachess.network;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
 
 public class Server{
 	private int port;
-
+	private ServerSocket server;
 	public Server(int port){
 		this.port = port;
+	}
+	
+	public void listen(){
+		try {
+			server = new ServerSocket(port);
+			Socket client = server.accept();
+			PrintWriter writer = new PrintWriter(client.getOutputStream());
+			writer.println("Welcome to our ITLA Chess Server");
+			writer.close();
+			client.close();			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
